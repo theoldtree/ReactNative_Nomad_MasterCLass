378a812d04bf68d34b046fc32d9c2df6d4f27b13
유정무 <yujeongmu@yujeongmuui-MacBookPro.local>
2022-02-10 18:24:25 +0900

    install query

--- App.js
+++ App.js
@@ -8,6 +8,9 @@ import { Ionicons } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
 import styled, { ThemeProvider } from 'styled-components/native'
 import { darkTheme, lightTheme } from './styles/styled';
+import { QueryClient, QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient();
 
 export default function App() {
   const [assets] = useAssets([require('./assets/greeting.png')])
@@ -19,11 +22,13 @@ export default function App() {
       <AppLoading />
     )
   return (
-    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-      <NavigationContainer>
-        <Tabs />
-      </NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <NavigationContainer>
+          <Tabs />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
